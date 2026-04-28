91123b313bdb16d1a010b1fcea28d029113af486
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-04-28 22:17:57 +0530

    add or update api docs

--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
 from fastapi import FastAPI
 from schema.schema import Post, PostResponse
 
-app = FastAPI()
+app = FastAPI(title="Todo API", description="This API only for development purpose", version="0.1.1")
 
-@app.get("/")
+@app.get("/", summary="Index", description="Index route for checking API health.")
 def index():
-    return {"msg": "something"}
+    return {"msg": "API running...."}
 
 
-@app.post("/create-post", response_model=PostResponse)
-def create_post(post: Post):
+@app.post("/create-post", response_model=PostResponse, summary="Create new post")
+def create_post(post: Post) -> PostResponse:
+    return PostResponse(post=post, message="Created")
+
+
+@app.put("/update-post/{post_id}", summary="Update Exisiting post")
+def update_post(post_id: str, post: Post):
     return {
-        "post": post,
-        "message": "Created"
+        "update_post": post,
+        "post_id": post_id,
+        "is_edited": True
     }
