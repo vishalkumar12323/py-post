5949f28e6d6e9c457ac356a3bd68a5e43eed219b
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-05-07 15:36:55 +0530

    Formate all api routes using APIRouter

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, APIRouter
 from schema.schema import TodoCreateResponse, Todo, TodoGetResponse, BaseResponse, UpdateTodoBody, TodoUpdateResponse
 from config.database import db
 from uuid import UUID
@@ -6,20 +6,25 @@ from uuid import UUID
 
 app = FastAPI(title="Todo App")
 
+get_router = APIRouter(prefix="/get", tags=["Get Router"])
+post_router = APIRouter(prefix="/post", tags=["Post Router"])
+put_router = APIRouter(prefix="/put", tags=["Put Router"])
+delete_router = APIRouter(prefix="/delete", tags=["Delete Router"])
+
 # Route for create new todo with unique id
-@app.post("/create-post", response_model=TodoCreateResponse)
+@post_router.post("/create-post", response_model=TodoCreateResponse, name="Create New Post")
 def create_post(todo: Todo):
     db.append(todo)
     return TodoCreateResponse(todo=todo, message="Created")
 
 # Route for fetch all todos
-@app.get("/todos", response_model=TodoGetResponse)
+@get_router.get("/todos", response_model=TodoGetResponse)
 def fetch_todos():
     return TodoGetResponse(todos=db, message="Success")
 
 
 # Route for fetch todo by its id
-@app.get("/todo/{todo_id}", response_model=Todo | BaseResponse)
+@get_router.get("/todo/{todo_id}", response_model=Todo | BaseResponse)
 def fetch_todo_by_id(todo_id: str) -> Todo | BaseResponse:
     try:
         todo_id = UUID(todo_id)
@@ -42,7 +47,7 @@ def find_todo(todo_id: str):
     return -1
 
 # Route for delete existing todo from the db
-@app.delete("/todo/{todo_id}")
+@delete_router.delete("/todo/{todo_id}")
 def delete_todo_by_id(todo_id:str):
     todo_idx = find_todo(todo_id=todo_id)
     print(todo_idx)
@@ -53,7 +58,7 @@ def delete_todo_by_id(todo_id:str):
 
 
 # Route for update todo using todo id
-@app.put("/update-todo/{todo_id}", response_model=TodoUpdateResponse | BaseResponse)
+@put_router.put("/update-todo/{todo_id}", response_model=TodoUpdateResponse | BaseResponse)
 def update_todo(todo_id: str, data: UpdateTodoBody) -> TodoUpdateResponse | BaseResponse:
     try:
         todo_id = UUID(todo_id)
@@ -73,7 +78,7 @@ def update_todo(todo_id: str, data: UpdateTodoBody) -> TodoUpdateResponse | Base
 
 
 # Route for fetch all categorial todos
-@app.get("/todos/search", response_model=TodoGetResponse|BaseResponse)
+@get_router.get("/todos/search", response_model=TodoGetResponse|BaseResponse, name="Fetch todos by category")
 def fetch_by_category(category: str) -> TodoGetResponse|BaseResponse:
     todos = [todo for todo in db if todo.category == category]
 
@@ -83,3 +88,9 @@ def fetch_by_category(category: str) -> TodoGetResponse|BaseResponse:
     return TodoGetResponse(todos=todos)
 
 
+
+routers = [get_router, post_router, put_router, delete_router]
+
+for rotuer in routers:
+    app.include_router(router=rotuer, prefix="/api")
+    
