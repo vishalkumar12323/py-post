1e37ae6966e120a142d621984ce510b2c6283b82
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-05-08 14:53:28 +0530

    add ratelimitting with slowapi

--- src/main.py
+++ src/main.py
@@ -1,28 +1,41 @@
-from fastapi import FastAPI, Request
-from time import perf_counter
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+# from time import perf_counter
 from routes.todo_routes import todo_router
 from routes.user_routes import user_router
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+from slowapi.middleware import SlowAPIMiddleware
 
 
 
 app = FastAPI(title="Todo App")
 
-@app.middleware("http")
-async def middleware(req: Request, next):
-    print(f"PATH:: {req["path"]}")
-    print(f"METHOD:: {req["method"]}")
+limiter = Limiter(key_func=get_remote_address)
 
-    payload = await req.body()
-    if payload:
-        print(payload)
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, lambda req, exe: JSONResponse(status_code=429, content={"detail": "To Many Request"}))
+
+# app.state.req_counter = 0
+
+# @app.middleware("http")
+# async def middleware(req: Request, next):
+#     print(f"PATH:: {req["path"]}")
+#     print(f"METHOD:: {req["method"]}")
+
+#     payload = await req.body()
+#     if payload:
+#         print(payload)
     
-    start = perf_counter()
-    request = await next(req)
+#     start = perf_counter()
+#     app.state.req_counter += 1
+#     request = await next(req)
 
-    end = perf_counter()
+#     end = perf_counter()
 
-    print(f"API TIME:: {start - end}")
-    return request
+#     print(f"API TIME:: {start - end}")
+#     return request
 
 routers = [todo_router, user_router]
 
--- src/routes/user_routes.py
+++ src/routes/user_routes.py
@@ -1,13 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from datetime import datetime
+from slowapi import Limiter
+from slowapi.util import get_remote_address
 
 user_router = APIRouter(prefix="/user", tags=["User APIs"])
 
+
+limiter = Limiter(key_func=get_remote_address)
+
+
 @user_router.post("/signup")
-def signup():
+@limiter.limit("2/minute")
+def signup(req: Request):
     return {
         "status": "success",
-        "user_id": str(datetime.second)
+        "user_count" : req.app.state.req_counter
     }
 
 
